88b23cb057706b0687d4e09d19b2af4e728ca5f8
vinicius evandro <vinnievandro@hotmail.com>
2022-03-16 01:51:33 -0300

    mesa feita 15/03

--- 08/mesa-de-trabalho/assets/js/main.js
+++ 08/mesa-de-trabalho/assets/js/main.js
@@ -1,1 +1,41 @@
 
+var documento = document.body;
+var tela = document.querySelector("#telaInicial");
+
+
+//! Segunda parte!!!!!!
+
+
+//! captura das teclas
+
+documento.addEventListener("keypress" , function(evento){ 
+
+
+//! Condicional pra saber se a tecla space foi pressionada
+
+
+    if(evento.code == "Space"){
+
+
+//! Ocultar a tela
+
+    tela.style.display = "none"
+
+    }
+
+    if(evento.code == "Enter"){
+
+    tela.style.display = "block"
+
+    } 
+});
+
+    //! Primeira parte !!!!!!!
+
+    var tabuleiro = document.querySelector(".tabuleiro");
+
+    tabuleiro.addEventListener("click", (evento) => evento.target.innerText = "x" );
+
+    tabuleiro.addEventListener("dblclick", (evento) => evento.target.innerText = "0" );
+
+    tabuleiro.addEventListener("mouseover", (evento) => console.log(evento));
