8e0753d94fa59e47eae061e70bf54ac015e0ad76
vinicius evandro <vinnievandro@hotmail.com>
2022-03-22 20:57:18 -0300

    mesa pronta

--- 10/app.js
+++ 10/app.js
@@ -1,22 +1,22 @@
 
 // 1. Seleção do formulário
-var formulario = document.getElementById('formularioDeCadastro');
+var formulário = document.getElementById('formulárioDeCadastro');
 
-// 2. Selação do campo
+// 2. Seleção do campo
 var campoTelefone = document.getElementById('telefone');
 
 // 3. Captura o evento de envio do formulário e disparar a rotina
 
-formulario.addEventListener('submit', function() {
+formulário.addEventListener('submit', function() {
     
     var valorDoCampoTelefoneSemTratamento = campoTelefone.value;
 
     // \s = espaços
     // \D = para números
-    var valorDoCampoTelefoneSemEspacos = valorDoCampoTelefoneSemTratamento
+    var valorDoCampoTelefoneSemEspaços = valorDoCampoTelefoneSemTratamento
     .replace(/\s/g, '');
 
-    var valorDoCampoTelefoneSemCaracteres = valorDoCampoTelefoneSemEspacos
+    var valorDoCampoTelefoneSemCaracteres = valorDoCampoTelefoneSemEspaços
     .replace(/\D/g, '');
 
     var textoMinusculo = valorDoCampoTelefoneSemTratamento.toLowerCase();
--- 11/mesa-de-trabalho/assets/js/main.js
+++ 11/mesa-de-trabalho/assets/js/main.js
@@ -1,25 +1,116 @@
 
-/* 
+    // 1. Selecione o formulário e o botão do campo de entrada
 
-    1. Selecione o formulário e o botão do campo de entrada
+    var campo = document.getElementById('formularioDeCadastro');
+    var botao = document.getElementById('botao');
+    
 
-    2. Crie uma rotina que será disparada a partir do evento de envio
+    // 2. Crie uma rotina que será disparada a partir do evento de envio
 
-        - 2.1. Remova todos os espaços vazios no início e no fim do texto
+    function validarEnviarDadosDoFormulario(evento) {
+    
+        var valorDoCampoNome = evento.target[0].value.trim();
+    
+        var campoEstaVazio = estaVazio(valorDoCampoNome);
+        var campoPossuiNumeros = possuiNumeros(valorDoCampoNome);
+        var campoPossuiCaracteresEspeciais = possuiCaracteresEspeciais(valorDoCampoNome);
+        var campoPossuiMaisDeVinteCaracteres = possuiMaisDeVinteCaracteres(valorDoCampoNome)
 
-        - 2.2. Valide se existem caracteres especiais
+    
 
-            - Se o campo possuir caracteres especiais, apresente um alerta para o usuário com a seguinte mensagem `"Não é permitido o uso de caracteres especiais em sua lista."`. **Nota:** Pode ser utilizado o método `alert()` para simplificar a lógica.
+        if(campoEstaVazio) {
+            console.log('Campo é obrigatório e precisa ser preenchido!');
+        }
+        else if (campoPossuiMaisDeVinteCaracteres) {
+            console.log('Campo não pode ultrapassar vinte caracteres');
+        }
+        else if(campoPossuiNumeros) {
+            console.log('Campo não pode ter números!');
+        }
+        else if(campoPossuiCaracteresEspeciais) {
+            console.log('Campo não pode conter caracteres especiais');
+        }
+        else {
+            console.log('Está tudo ok para envio');
+        }
 
-        - 2.3. Caso passe na validacão, apresente o item no elemento DOM `<li class="c-lista__item">`.
 
-    3. Crie uma rotina que será disparada sempre que o usuário tirar o foco da campo de entrada
 
-        - 2.1. Valide se o campo esta vazio
+    };    
 
-            - Se o campo estiver vazio, desative o botão `<button class="c-form__botao u-my" type="submit">Adicionar item</button>`.
 
-        - 2.2. Valide se existem caracteres especiais 
+    function possuiCaracteresEspeciais(valorDoCampo) {
 
-            - Se o campo possuir caracteres especiais, apresente a seguinte mensagem no console do navegador `"Não é permitido o uso de caracteres especiais em sua lista."`.
-*/
+        var expressaoRegular = /\W/g;
+                
+        var respostaDaValidacao = expressaoRegular.test(valorDoCampo);
+        
+        return respostaDaValidacao;
+    }
+    
+    function estaVazio(valorDoCampo) {
+
+        var respostaDaValidacao = valorDoCampo === '';
+        
+        return respostaDaValidacao;
+    }
+    
+
+    function possuiNumeros(valorDoCampo) {
+    
+        var expressaoRegular = /\d+/g;
+            
+        var respostaDaValidacao = expressaoRegular.test(valorDoCampo);
+    
+        return respostaDaValidacao;
+    }
+    
+
+    
+   
+    function possuiMaisDeVinteCaracteres(valorDoCampo) {
+    
+        var numeroDeCaracteres = valorDoCampo.length;
+    
+        var limiteDeCaracteres = 20;
+    
+        var respostaDaValidacao = numeroDeCaracteres > limiteDeCaracteres;
+    
+        return respostaDaValidacao;
+        
+    }
+    
+    campo.addEventListener('blur', validarEnviarDadosDoFormulario);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     - 2.2. Valide se existem caracteres especiais
+
+    //         - Se o campo possuir caracteres especiais, apresente um alerta para o usuário com a seguinte mensagem `"Não é permitido o uso de caracteres especiais em sua lista."`. **Nota:** Pode ser utilizado o método `alert()` para simplificar a lógica.
+
+    //     - 2.3. Caso passe na validacão, apresente o item no elemento DOM `<li class="c-lista__item">`.
+
+    // 3. Crie uma rotina que será disparada sempre que o usuário tirar o foco da campo de entrada
+
+    //     - 2.1. Valide se o campo esta vazio
+
+    //         - Se o campo estiver vazio, desative o botão `<button class="c-form__botao u-my" type="submit">Adicionar item</button>`.
+
+    //     - 2.2. Valide se existem caracteres especiais 
+
+    //         - Se o campo possuir caracteres especiais, apresente a seguinte mensagem no console do navegador `"Não é permitido o uso de caracteres especiais em sua lista."`.
